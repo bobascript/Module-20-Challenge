5f0588bd9008992dfa099e97f2a9198cad0e34a1
mint <mintghostii@mints-MacBook-Pro.local>
2023-03-01 12:27:07 -0500

    Finished adding the Portfolio section. Added images for the section to assets folder.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,71 @@
 import React from "react";
+import "../index.css";
+import weather from "../assets/weatherdashboard2.png";
+import quiz from "../assets/quizscreenshot.png";
+import pwgen from "../assets/pwgen.png";
+import accessibility from "../assets/mod1screenshot.png"
+import foodgram from "../assets/foodgram.png";
+import secretsauce from "../assets/secretsauce.png";
+
 
 function Portfolio(){
-    const portfolio =[]
+    const portfolio =[
+        {
+            name: "Weather Dashboard",
+            imgsrc: weather,
+            link: "https://bobascript.github.io/weather-dashboard/",
+            repo: "https://github.com/bobascript/weather-dashboard"
+        },
+        {
+            name: "Code Quiz",
+            imgsrc: quiz,
+            link: "https://bobascript.github.io/4-code-quiz/",
+            repo: "https://github.com/bobascript/4-code-quiz"
+        },
+        {
+            name: "Password Generator",
+            imgsrc: pwgen,
+            link: "https://bobascript.github.io/password-generator-fixed/",
+            repo: "https://github.com/bobascript/password-generator-fixed"
+        },
+        {
+            name: "Module 1 Challenge: Accessibility",
+            imgsrc: accessibility,
+            link: "https://bobascript.github.io/module1challenge-accessibility/",
+            repo: "https://github.com/bobascript/module1challenge-accessibility"
+        },
+        {
+            name: "Foodgram",
+            imgsrc: foodgram,
+            link: "https://bulkacity.github.io/foodgram/",
+            repo: "https://github.com/bulkacity/FoodGram"
+        },
+        {
+            name: "Secret Sauce",
+            imgsrc: secretsauce,
+            link: "https://damp-river-14558.herokuapp.com/",
+            repo: "https://github.com/graciaan/SecretSauce"
+        },
+        
+
+    ]
 
     return (
         <div>
+            <section className="middle">
+             <h2>My Work</h2>
+            <div id="portitem">
+                {portfolio.map((portfolio) => (
+                    <div key={portfolio.name}>
+                        <div className="card"
+                            style={{ backgroundImage: `url(${portfolio.imgsrc})` }}>
+                            <a target="_blank" href={portfolio.link}>{portfolio.name}</a>
+                            <a target="_blank" href={portfolio.repo}>Github</a>
+                        </div>
+                    </div>
+                ))}
+            </div>   
+            </section>
             
         </div>
     )
